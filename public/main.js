4f8ee20c6ff2276c0997525f9224ea280d37390c
stephanoula <stephprodromos@gmail.com>
2017-08-29 20:01:51 -0600

    five

--- public/main.js
+++ public/main.js
@@ -1,25 +1,25 @@
-var items = [
-    {
-        item: 'water bottles',
-        weight: 1,
-        cost: 1.5,
-    },
-    {
-        item: 'car tires',
-        weight: 22,
-        cost: 80,
-    },
-    {
-        item: 'shoes',
-        weight: 2.5,
-        cost: 35,
-    },
-    {
-        item: 'snacks',
-        weight: 0.33,
-        cost: 3,
-    }
-]
+//var items = [
+//    {
+//        item: 'water bottles',
+//        weight: 1,
+//        cost: 1.5,
+//    },
+//    {
+//        item: 'car tires',
+//        weight: 22,
+//        cost: 80,
+//    },
+//    {
+//        item: 'shoes',
+//        weight: 2.5,
+//        cost: 35,
+//    },
+//    {
+//        item: 'snacks',
+//        weight: 0.33,
+//        cost: 3,
+//    }
+//]
 
 $(document).ready(function() {
   var waterCount = 0
@@ -28,12 +28,16 @@ $(document).ready(function() {
   var snackCount = 0
   var totalPriceCount = 0
   var totalWeightCount = 0
+  //add total count
+  var totalCountP = [{item: water, price: 1.5}, {item: tire, price: 80}, {item: shoe, price: 35}, {item: snack, price: 3}];
+  var totalCountW = [{item: water, weight: 1}, {item: tire, weight: 22}, {item: shoe, weight: 2.5}, {item: snack, weight: 1}];
   $("#waterCounter").text("Count: "+waterCount)
   $("#tireCounter").text("Count: "+tireCount)
   $("#shoeCounter").text("Count :"+shoeCount)
   $("#snackCounter").text("Count :"+snackCount)
   $(".totalPriceCount").text("Total Price: $"+totalPriceCount)
   $(".totalWeightCount").text("Total Weight: "+totalWeightCount +" lbs")
+    
   $("#waterIncrease").click(function() {
     waterCount++
     totalPriceCount += 1.5
@@ -41,6 +45,9 @@ $(document).ready(function() {
     $("#waterCounter").text("Count: "+waterCount)
     $(".totalPriceCount").text("Total Price: $"+totalPriceCount)
     $(".totalWeightCount").text("Total Weight: "+totalWeightCount + " lbs")
+  if (totalWeightCount > 200 || totalPriceCount > 200) {
+      $('#validate').addClass('invalid')
+  }
   })
   $("#waterDecrease").click(function() {
     waterCount--
@@ -50,6 +57,7 @@ $(document).ready(function() {
     $(".totalPriceCount").text("Total Price: $"+totalPriceCount)
     $(".totalWeightCount").text("Total Weight: "+totalWeightCount + " lbs")
   })
+    
   $("#tireIncrease").click(function() {
     tireCount++
     totalPriceCount += 80
@@ -99,14 +107,26 @@ $(document).ready(function() {
     $(".totalWeightCount").text("Total Weight: "+totalWeightCount + " lbs")
   })
   $("#validate").click(function() {
+      $.post(`/validate-cargo`, {price: totalPriceCount, weight: totalWeightCount} function(data){
+        //send info back
+          console.log(data);
+          $("#validateP").text(data)
     if (totalPriceCount > 200 && totalWeightCount > 200) {
       $("#validateP").text("Oops! Your have gone over your total weight limit of 200 lbs and price limit of $200. Please make changes accordingly.")
     } else if (totalWeightCount > 200) {
       $("#validateP").text("Oops! Your have gone over your total weight limit of 200 lbs. Please make changes accordingly.")
     } else if(totalPriceCount > 200) {
       $("#validateP").text("Oops! Your have gone over your total price limit of $200. Please make changes accordingly.")
+        //FIX THIS
+//    } else if (totalCountP > 200 && totalCountW > 200){
+//      $("#validateP").text("Oops! Your have gone over your total limit of $200 and 200 lbs for all items. Please make changes accordingly.")
+//    } else if (totalCountP > 200 ){
+//      $("#validateP").text("Oops! Your have gone over your total limit of $200 for all items. Please make changes accordingly.")
+//    } else if (totalCountW > 200){
+//      $("#validateP").text("Oops! Your have gone over your total limit of 200 lbs for all items. Please make changes accordingly.")
     } else {
-      $("#validateP").text('')
+          $("#validateP").text(' ')
+      })
     }
   })
 })
--- server.js
+++ server.js
@@ -1,30 +1,52 @@
 var express = require('express');
 //var cargo = require('cargo-validator');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static('./public'));
-
+app.use(bodyParser.json());
 
 
 app.get('/', function(req, res){
-    res.sendFile('/cargo.html', {root: './public'})
+    res.sendFile('/boston.html', {root: './public'})
+})
+
+app.get('/nyc', function(req, res){
+    res.sendFile('/nyc.html', {root: './public'})
+})
+
+app.get('/chicago', function(req, res){
+    res.sendFile('/chicago.html', {root: './public'})
+})
+
+app.get('/boulder', function(req, res){
+    res.sendFile('/boulder.html', {root: './public'})
+})
+
+app.get('/sanfran', function(req, res){
+    res.sendFile('/sanfran.html', {root: './public'})
 })
 
 app.post(`/validate-cargo`, function(req, res){
-    $("#validate").click(function() {
-    if (totalPriceCount > 200 && totalWeightCount > 200) {
-      $("#validateP").text("Oops! Your have gone over your total weight limit of 200 lbs and price limit of $200. Please make changes accordingly.")
-    } else if (totalWeightCount > 200) {
-      $("#validateP").text("Oops! Your have gone over your total weight limit of 200 lbs. Please make changes accordingly.")
-    } else if(totalPriceCount > 200) {
-      $("#validateP").text("Oops! Your have gone over your total price limit of $200. Please make changes accordingly.")
+    console.log('body: ', req.body)
+    //price and weight: form key names in main.js when sending it to server
+    console.log('prices: ' , req.body.price)
+    console.log('weights: ', req.body.weight)
+    //same logic as in main.js
+    if (req.body.price > 200 && req.body.weight > 200) {
+      res.send("Oops! Your have gone over your total weight limit of 200 lbs and price limit of $200. Please make changes accordingly.")
+    } else if (req.body.weight > 200) {
+      res.send("Oops! Your have gone over your total weight limit of 200 lbs. Please make changes accordingly.")
+    } else if(req.body.price > 200) {
+      res.send("Oops! Your have gone over your total price limit of $200. Please make changes accordingly.")
     } else {
-      $("#validateP").text('');
       res.send("Cargo Validated!");
     }
 })
     
     //send stuff from server back to client
 
-app.listen(8000);
+app.listen(8080, function(){
+    console.log('Server started on 8080')
+});
